60f1e3d8281b50f44bb110a0190a78a4a6d4825f
H winesf <winesf@bk.ru>
2020-12-11 12:57:21 +0200

    Finish resize table

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,20 +3,26 @@ const CODES = {
   Z: 90,
 };
 
-function toCell() {
+function toCell(_, col) {
   return `
-      <div class="cell" contenteditable></div>
+      <div class="cell" contenteditable data-col="${col}"></div>
   `;
 }
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-  <div class="column">${col}</div>
+  <div class="column" data-type="resizable" data-col="${index}">
+    ${col}
+    <div class="col-resize" data-resize="col"></div>
+</div>
   `;
 }
 function createRow(index, content) {
+  const resizer = index ? `<div class="row-resize" data-resize="row"></div>`:'';
   return `
-    <div class="row">
-      <div class="row-info">${index ? index : ''}</div>
+    <div class="row" data-type="resizable">
+      <div class="row-info">${index ? index : ''}
+        ${resizer}
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `;
@@ -36,7 +42,7 @@ export function createTable(rowsCount = 15) {
       .map(toColumn)
       .join('');
 
-  rows.push(createRow('info', cols));
+  rows.push(createRow(null, cols));
 
   for (let i = 0; i < rowsCount; i++) {
     const cells = new Array(colsCount)
