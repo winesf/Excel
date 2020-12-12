bb5c659215675e127a9127442292723d6ba9f57c
H winesf <winesf@bk.ru>
2020-12-12 16:03:27 +0200

    Finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,10 +5,20 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emitter = options.emitter;
+    this.uncubscribes = [];
+    this.prepare();
   }
-  // Возвращает-шаблон-компонента
+  prepare() { }
   toHTML() {
     return '';
+  }
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.uncubscribes.push(unsub);
   }
   init() {
     this.initDOMListeners();
@@ -16,5 +26,6 @@ export class ExcelComponent extends DomListener {
 
   destroy() {
     this.removeDOMListeners();
+    this.uncubscribes.forEach((unsub) => unsub());
   }
 }
