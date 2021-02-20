8b10851ed30922376b1d0a4a9a626e8089b61995
wines <wines@bk.ru>
2021-02-21 01:38:42 +0300

    Release 1.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
         new CopyPlugin({
             patterns: [
                 { from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist ')
+                    to: path.resolve(__dirname, 'dist')
                 }]
         }),
         new MiniCssExtractPlugin({
